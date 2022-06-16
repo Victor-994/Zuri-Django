23868948b47c22bd31f1223a8fb0c5e02d25ab0d
Victor-994 <victoradejumo2@gmail.com>
2022-06-16 13:36:02 +0100

    first commit

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    publish_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+    
